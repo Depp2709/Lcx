61fc839a635497d08f6649c28a2dae2ca65e66a8
Depp2709 <akmalmranggen@gmail.com>
2025-04-08 15:42:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-document.getElementById('generateButton').addEventListener('click', function() {
-    const urlInput = document.getElementById('urlInput').value;
-    
-    // Validasi URL
-    if (!urlInput) {
-        alert('Silakan masukkan URL yang valid.');
-        return;
-    }
+let map;
+let marker;
 
-    // Buat link tracker (misalnya, menambahkan parameter query)
-    const trackerLink = `${urlInput}?tracker=${new Date().getTime()}`;
-    
-    // Tampilkan link tracker
-    document.getElementById('trackerLink').innerText = trackerLink;
-});
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: { lat: -6.200000, lng: 106.816666 }, // Koordinat Jakarta sebagai contoh
+    });
+
+    marker = new google.maps.Marker({
+        position: { lat: -6.200000, lng: 106.816666 }, // Lokasi awal
+        map: map,
+        title: "Lokasi Tracker",
+    });
+
+    // Inisialisasi WebSocket
+    const socket = new WebSocket('ws://localhost:8080');
+
+    socket.onmessage = function(event) {
+        const location = JSON.parse(event.data);
+        updateMarker(location);
+    };
+}
+
+function updateMarker(location) {
+    const newPosition = { lat: location.lat, lng: location.lng };
+    marker.setPosition(newPosition);
+    map.setCenter(newPosition);
+}
+
+// Panggil fungsi initMap saat halaman dimuat
+window.onload = initMap;
